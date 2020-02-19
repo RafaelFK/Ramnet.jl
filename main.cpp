f6f04f35866f834aef9948bf1ea6e72f1bf5e5a5
Rafael F. Katopodis <rafaelkatopodis@gmail.com>
2020-02-19 12:46:12 -0300

    Testing RAM memory initialization through Hamming Weight (should be zeroed out)

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include "ram_node.hpp"
-#include "ram_discriminator.hpp"
 
-int neuron_test() {
-  ramnet::RAMNode<3> neuron {};
+int main() {
+  ramnet::RAMNode neuron {3};
 
+  std::cout << neuron.size() << std::endl;
+  std::cout << "Hamming Weight: " << neuron.hammingWeight() << std::endl;
+  
   neuron.train(0b000);
 
   std::cout << "Trained pattern: " << neuron.fire(0b000) << std::endl;
   std::cout << "Untrained pattern: " << neuron.fire(0b001) << std::endl;
 }
 
-int main() {
-  ramnet::RAMDiscriminator<8, 2> classifier;
+int discriminator_test() {
+  // ramnet::RAMDiscriminator<8, 2> classifier;
 
-  classifier.train(0b11100100);
+  // classifier.train(0b11100100);
 
-  std::cout << "Score: " << classifier.score(0b11100100) << std::endl;
-  std::cout << "Score: " << classifier.score(0b01111000) << std::endl;
+  // std::cout << "Score: " << classifier.score(0b11100100) << std::endl;
+  // std::cout << "Score: " << classifier.score(0b01111000) << std::endl;
 }
