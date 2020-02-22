e0fc3fe85e15d3f9dd6c1801f55c1c45f3118a5a
Rafael F. Katopodis <rafaelkatopodis@gmail.com>
2020-02-22 01:02:48 -0300

    ram_discriminator test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "../include/ram_node.hpp"
+#include "../include/ram_discriminator.hpp"
 
-int main() {
+int neuron_test() {
   ramnet::RAMNode neuron {3};
 
   std::cout << neuron.size() << std::endl;
@@ -13,11 +14,11 @@ int main() {
   std::cout << "Untrained pattern: " << neuron.fire(0b001) << std::endl;
 }
 
-int discriminator_test() {
-  // ramnet::RAMDiscriminator classifier {8, 2};
+int main() {
+  ramnet::RAMDiscriminator classifier {8, 2};
 
-  // classifier.train(0b11100100);
+  classifier.train(0b11100100);
 
-  // std::cout << "Score: " << classifier.score(0b11100100) << std::endl;
-  // std::cout << "Score: " << classifier.score(0b01111000) << std::endl;
+  std::cout << "Score: " << classifier.score(0b11100100) << std::endl;
+  std::cout << "Score: " << classifier.score(0b01111000) << std::endl;
 }
