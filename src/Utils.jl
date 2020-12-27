a97f3baf77a6e3195903efbdbba2763133404613
Rafael F. Katopodis <rafaelkatopodis@gmail.com>
2020-12-27 15:20:29 -0300

    Documenting Utils module

--- src/Utils.jl
+++ src/Utils.jl
@@ -2,34 +2,77 @@ module Utils
 
 export stack, ambiguity
 
-# function check_length_consistency(input::AbstractVector) end
+"""
+    check_length_consistency(inputs::AbstractVector...)
 
+Raise a `DimensionMismatch` exception if the input vectors are not all the same
+length. Does nothing otherwise.
+
+# Examples
+
+```jldoctest
+julia> check_length_consistency([1,2,3], [4,5])
+ERROR: DimensionMismatch("Input vectors should have the same length!")
+[...]
+```
+"""
 function check_length_consistency(inputs::AbstractVector...)
     if any(!=(length(first(inputs))), map(length, inputs))
         throw(DimensionMismatch("Input vectors should have the same length!"))
     end
 end
 
+"""
+    check_length_consistency(inputs::AbstractMatrix...)
+
+Raise a `DimensionMismatch` exception if the input matrices don't have the same
+number of columns. Does nothing otherwise.
+
+# Examples
+
+```jldoctest
+julia> check_length_consistency([1 2 3; 4 5 6], [7 8; 9 10])
+ERROR: DimensionMismatch("Input vectors should have the same length!")
+[...]
+```
+"""
 function check_length_consistency(inputs::AbstractMatrix...)
     if any(!=(size(first(inputs))[2]), map(i -> size(i)[2], inputs))
-        throw(DimensionMismatch("Input vectors should have the same length!"))
+        throw(DimensionMismatch("input matrices should have the same number of columns"))
     end
 end
 
 # TODO: Consider renaming it to stack_rows
+"""
+    stack(inputs::AbstractVector...)
+
+Stack the input vectors as rows of a matrix.
+
+# Examples
+```jldoctest
+julia> stack([1,2,3], [4,5,6])
+2×3 Array{Int64,2}:
+ 1  2  3
+ 4  5  6
+```
+"""
 function stack(inputs::AbstractVector...)
     check_length_consistency(inputs...)
 
     mapreduce(permutedims, vcat, inputs)
 end
 
+"""
+    stack(inputs::AbstractMatrix...)
+
+Stack the input matrices vertically.
+"""
 function stack(inputs::AbstractMatrix...)
     check_length_consistency(inputs...)
 
     reduce(vcat, inputs)
 end
 
-# TODO: Vectors must have the same length
 # TODO: Untested
 function accuracy(y_predict::AbstractVector{T}, y_actual::AbstractVector{T}) where {T}
     check_length_consistency(y_predict, y_actual)
@@ -37,6 +80,12 @@ function accuracy(y_predict::AbstractVector{T}, y_actual::AbstractVector{T}) whe
     sum(y_predict .== y_actual) / length(y_predict)
 end
 
+"""
+    hasdraw(X::Tuple)
+
+Return `true` if there is draw in the tuple `X` and `false` otherwise. A draw 
+occurs when the largest value in the tuple is non-unique.
+"""
 function hasdraw(X::Tuple)
     length(X) == 0 && throw(ArgumentError("X must have at least one element."))
 
@@ -55,8 +104,18 @@ function hasdraw(X::Tuple)
     return repeats != 1
 end
 
-# TODO: Vectors must have the same length
+# TODO: Add example to docstring
+"""
+    ambiguity(responses...)
+
+Measure the ambiguity in a multidiscriminator classifier prediction.
+
+The ambiguity is the average number of ties in the responses of the
+discriminators that compose a multidiscriminator classifier when predicting the
+classes of a test set.
+"""
 function ambiguity(responses::AbstractVector{T}...) where {T <: Integer}
+    check_length_consistency(responses...)
     count = 0
     
     for res in Iterators.zip(responses...)
@@ -67,9 +126,5 @@ function ambiguity(responses::AbstractVector{T}...) where {T <: Integer}
 
     return count / (length ∘ first)(responses)
 end
-
-# function ambiguity(model, X::T) where {T <: AbstractMatrix{Bool}}
-#     ambiguity((values ∘ predict_response)(model, X)...)
-# end
 
 end
